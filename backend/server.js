c2f75fd1eabc2c9f06b9326363f966f4dccdcf64
Sithum Wijesinghe <sithumwijesinghe59@gmail.com>
2024-10-27 08:39:16 +0530

    intigrate routes with main server file

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import userRoutes from './routes/userRoutes.js';
+import propertyRoutes from './routes/propertyRoutes.js';
 
 dotenv.config();
 const app = express();
 
+
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/properties', propertyRoutes);
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Real Estate Marketplace Backend');
