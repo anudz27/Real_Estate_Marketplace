2da37f34c469a2a4f059c5a6d69908a239b58cf5
Shashini Dias <117101873+anudz27@users.noreply.github.com>
2024-11-15 15:50:15 +0530

    nav bar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,20 +5,24 @@ import '../styles/Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <h1 className="navbar-logo">Real Estate Marketplace</h1>
-            <ul className="navbar-links">
-                <li>
-                    <Link to="/">Home</Link>
+        <nav className="navbar flex justify-between items-center px-8 py-4 bg-white text-black">
+            <h1 className="navbar-logo font-outfit text-xl">UrbanNest</h1>
+            <ul className="navbar-links flex list-none items-center ">
+                <li className='ml-5'>
+                    <Link to="/" className="text-black font-semibold font-outfit text-lg no-underline hover:text-[#FADC07]">Buy</Link>
                 </li>
-                <li>
-                    <Link to="/signup">Sign Up</Link>
+                <li className='ml-5'>
+                    <Link to="/signup" className="text-black font-semibold font-outfit text-lg no-underline hover:text-[#FADC07]">Rent</Link>
                 </li>
-                <li>
-                    <Link to="/login">Login</Link>
+                <li className='ml-5'>
+                    <Link to="/login" className="text-black font-semibold font-outfit text-lg no-underline hover:text-[#FADC07]">Agent</Link>
                 </li>
-                <li>
-                    <Link to="/properties">Properties</Link>
+                <li className='ml-5 '>
+                    <Link to="/properties" className="text-black font-semibold font-outfit text-lg no-underline hover:text-[#FADC07]">Blog</Link>
+                </li>
+                <li className='ml-40'>
+                    <button className="bg-[#FADC07] text-black font-semibold font-outfit text-lg px-4 py-2 rounded-lg hover:bg-[#ffff]">Sign In</button>
+                   
                 </li>
             </ul>
         </nav>
