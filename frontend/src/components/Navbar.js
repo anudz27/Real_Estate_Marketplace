cda52d557d527dab4e6b9e339c1cb4c1d58c2c6f
Shashini Dias <117101873+anudz27@users.noreply.github.com>
2024-11-15 16:15:59 +0530

    add nav an home

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,32 @@
+// src/components/Navbar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar flex justify-center items-center px-8 py-4 bg-white text-black">
+            <h1 className="navbar-logo font-outfit text-xl">UrbanNest</h1>
+            <ul className="navbar-links flex list-none items-center ">
+                <li className='ml-5'>
+                    <Link to="/" className="text-black font-semibold font-outfit text-lg no-underline hover:text-[#FADC07]">Buy</Link>
+                </li>
+                <li className='ml-5'>
+                    <Link to="/signup" className="text-black font-semibold font-outfit text-lg no-underline hover:text-[#FADC07]">Rent</Link>
+                </li>
+                <li className='ml-5'>
+                    <Link to="/login" className="text-black font-semibold font-outfit text-lg no-underline hover:text-[#FADC07]">Agent</Link>
+                </li>
+                <li className='ml-5 '>
+                    <Link to="/properties" className="text-black font-semibold font-outfit text-lg no-underline hover:text-[#FADC07]">Blog</Link>
+                </li>
+                <li className='ml-40'>
+                    <button className="bg-[#FADC07] text-black font-semibold font-outfit text-lg px-4 py-2 rounded-lg hover:bg-[#ffff]">Sign In</button>
+                   
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
