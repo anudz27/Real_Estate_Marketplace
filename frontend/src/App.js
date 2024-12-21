13dd27b0a3ad47a69f4984f97319240152dbb996
Shashini Dias <117101873+anudz27@users.noreply.github.com>
2024-12-21 10:35:23 +0530

    init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import SignUp from './components/SignUp';
+import SignIn from './components/signIn';
+import Home from './pages/home/home';
 
 function App() {
   return (
