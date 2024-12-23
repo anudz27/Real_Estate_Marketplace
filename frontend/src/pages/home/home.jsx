632cb7d4fc7beb065a7a436344dd4955f3463dbc
Shashini Dias <117101873+anudz27@users.noreply.github.com>
2024-12-23 22:16:06 +0530

    init

---
+++ frontend/src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from '../../assets/images/home.jpg';
+
+const Home = () => {
+  return (
+    <div>
+      <div className="container">
+        <img src={Home} alt="Home" className="img-fluid" />
+      </div>
+    </div>
+  );
+}
+export default Home;
