57a276469b8ec9903446c07ef9e4eac27c1b2d9e
Shashini Dias <117101873+anudz27@users.noreply.github.com>
2025-02-14 13:58:18 +0530

    Properties page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../styles/header.css"; // Ensure this path is correct
 import HImg from "../assets/img/4.jpg";
+import Search from "./search";
 
 const Header = () => {
   return (
     <section
-      className="header w-11/12 bg-cover bg-center bg-no-repeat rounded-2xl mx-auto p-8 flex flex-col justify-center items-center"
+      className="header w-11/12 max-h-screen bg-cover bg-center bg-no-repeat rounded-2xl mx-auto p-8 flex flex-col justify-center items-center"
       id="header"
       style={{
         backgroundImage: `url(${HImg})`,
@@ -21,61 +22,8 @@ const Header = () => {
       <a href="#search" className="btn font-bold py-2 px-4 rounded-md hover:text-yellow-500">To Rent</a>
       </div>
     
-
-      <div className="container bg-white bg-opacity-80 rounded-lg p-6 shadow-lg w-full max-w-4xl mt-20">
-      <form action="" className="grid grid-cols-1 md:grid-cols-4 gap-4">
-  <div className="box">
-    <select
-      className="w-full p-2 border border-gray-300 rounded-md"
-      defaultValue=""
-    >
-      <option value="" disabled>
-        Select Location
-      </option>
-      <option value="new-york">Colombo</option>
-      <option value="los-angeles">Galle</option>
-      <option value="chicago">Kandy</option>
-      <option value="miami">Matara</option>
-    </select>
-  </div>
-  <div className="box">
-    <select
-      className="w-full p-2 border border-gray-300 rounded-md"
-      defaultValue=""
-    >
-      <option value="" disabled>
-        Select Property Type
-      </option>
-      <option value="apartment">Apartment</option>
-      <option value="house">House</option>
-      <option value="villa">Villa</option>
- 
-    </select>
-  </div>
-  <div className="box">
-    <select
-      className="w-full p-2 border border-gray-300 rounded-md"
-      defaultValue=""
-    >
-      <option value="" disabled>
-        Select Price Range
-      </option>
-      <option value="0-50000">Rs0 - Rs50,000</option>
-      <option value="50000-100000">Rs50,000 - Rs100,000</option>
-      <option value="100000-200000">Rs100,000 - Rs200,000</option>
-      <option value="200000-plus">Rs200,000+</option>
-    </select>
-  </div>
-  <div className="box">
-    <button
-      className="btn w-full p-2 col-span-1 md:col-span-4 bg-yellow-400 text-white font-bold py-2 px-4 rounded-md hover:bg-yellow-500"
-    >
-      Search
-    </button>
-  </div>
-</form>
-
-      </div>
+      <Search/>
+    
       
     </section>
   );
