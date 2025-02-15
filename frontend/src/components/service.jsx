a1f8b2076c7fbc866e6c4a2fd98444300d9800dc
Shashini Dias <117101873+anudz27@users.noreply.github.com>
2025-02-15 16:27:43 +0530

    service section

---
+++ frontend/src/components/service.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <div className='container mx-auto my-20 px-12 text-black w-11/12 rounded-2xl p-8'>
+              <h1 className='text-4xl font-outfit font-semibold mt-20 text-center tracking-widest'>  Explore Topics </h1>
+
+              
+        </div>
+    );
+}
+
+export default Service;
